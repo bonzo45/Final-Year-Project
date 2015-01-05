d636d19622baec18daa542dd460776af75b4d752
Sam Esgate <sam.esgate11@imperial.ac.uk>
2015-01-05 20:45:26 +0000

    ITK & VTK are driving me mad. Why won't you play nice...

---
+++ VTK-Examples/HAVSVolumeMapper/CMakeFiles/HAVSVolumeMapper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/HAVSVolumeMapper.dir/HAVSVolumeMapper.cxx.o"
+  "HAVSVolumeMapper.pdb"
+  "HAVSVolumeMapper"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/HAVSVolumeMapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
