c1e89605cf0f7e7159daf2fd2fe71eae84cc4860
Sam Esgate <sam.esgate11@imperial.ac.uk>
2015-01-06 12:01:15 +0000

    Read through and commented the example. Now to extend it.

--- ImageToVTKImageFilter/ImageToVTKImageFilter.cxx
+++ ImageToVTKImageFilter/ImageToVTKImageFilter.cxx
@@ -13,45 +13,46 @@
 #include "itkRGBPixel.h"
 
 int main(int argc, char *argv[]) {
+    // Print Usage if no Image supplied.
     if (argc < 2) {
         std::cerr << "Usage: " << std::endl;
         std::cerr << argv[0] << " inputImageFile" << std::endl;
         return EXIT_FAILURE;
     }
 
+    // Define Types
     typedef itk::Image<itk::RGBPixel<unsigned char>, 2> ImageType;
     typedef itk::ImageFileReader<ImageType>             ReaderType;
     typedef itk::ImageToVTKImageFilter<ImageType>       ConnectorType;
 
+    // Create a 'Reader' for the Image. (ITK)
+    // Create a 'Connector'. (ITK -> VTK)
     ReaderType::Pointer reader = ReaderType::New();
     ConnectorType::Pointer connector = ConnectorType::New();
-
     reader->SetFileName(argv[1]);
     connector->SetInput(reader->GetOutput());
   
-    vtkSmartPointer<vtkImageActor> actor =
-    vtkSmartPointer<vtkImageActor>::New();
-#if VTK_MAJOR_VERSION <= 5
-    actor->SetInput(connector->GetOutput());
-#else
+    // Create an 'actor' to represent the image.
+    // Actors are things that can be rendered.
+    vtkSmartPointer<vtkImageActor> actor = vtkSmartPointer<vtkImageActor>::New();
     connector->Update();
     actor->GetMapper()->SetInputData(connector->GetOutput());
-#endif
+    
+    // Create a renderer and add that actor.
     vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
     renderer->AddActor(actor);
     renderer->ResetCamera();
 
+    // Create a render window.
     vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
     renderWindow->AddRenderer(renderer);
 
+    // Create an interactor.
     vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
     vtkSmartPointer<vtkInteractorStyleImage> style = vtkSmartPointer<vtkInteractorStyleImage>::New();
-
     renderWindowInteractor->SetInteractorStyle(style);
-
     renderWindowInteractor->SetRenderWindow(renderWindow);
     renderWindowInteractor->Initialize();
-
     renderWindowInteractor->Start();
   
     return EXIT_SUCCESS;
