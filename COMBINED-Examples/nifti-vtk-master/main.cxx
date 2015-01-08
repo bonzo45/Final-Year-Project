4285e735f22061758749e91460bd4ee44dd80694
Sam Esgate <sam.esgate11@imperial.ac.uk>
2015-01-08 18:29:59 +0000

    OK, slices work (in a separate window). It looks like I'm going to need to use Qt to get it to play nicely on the same screen but that would have had to happen eventually anyway. Will re-factor the interactor before moving to QT. Might be a job for tomorrow.

--- COMBINED-Examples/nifti-vtk-master/main.cxx
+++ COMBINED-Examples/nifti-vtk-master/main.cxx
@@ -10,7 +10,6 @@
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkVolumeProperty.h>
-#include <vtkMatrix4x4.h>
 #include <vtkAxesActor.h>
 
 #include <vtkImageSlice.h>
@@ -18,9 +17,116 @@
 #include <vtkImageResliceMapper.h>
 #include <vtkInteractorStyleImage.h>
 
+#include <vtkImageViewer2.h>
+#include <vtkTextMapper.h>
+#include <vtkTextProperty.h>
+#include <vtkActor2D.h>
+
+#include <vtkObjectFactory.h>
+
 const float BACKGROUND_R = 0.5f;
 const float BACKGROUND_G = 0.5f;
 const float BACKGROUND_B = 1.0f;
+
+// helper class to format slice status message
+class StatusMessage {
+public:
+   static std::string Format(int slice, int maxSlice) {
+      std::stringstream tmp;
+      tmp << "Slice Number  " << slice + 1 << "/" << maxSlice + 1;
+      return tmp.str();
+   }
+};
+ 
+ 
+// Define own interaction style
+class myVtkInteractorStyleImage : public vtkInteractorStyleImage
+{
+public:
+   static myVtkInteractorStyleImage* New();
+   vtkTypeMacro(myVtkInteractorStyleImage, vtkInteractorStyleImage);
+ 
+protected:
+   vtkImageViewer2* _ImageViewer;
+   vtkTextMapper* _StatusMapper;
+   int _Slice;
+   int _MinSlice;
+   int _MaxSlice;
+ 
+public:
+   void SetImageViewer(vtkImageViewer2* imageViewer) {
+      _ImageViewer = imageViewer;
+      _MinSlice = imageViewer->GetSliceMin();
+      _MaxSlice = imageViewer->GetSliceMax();
+      _Slice = _MinSlice;
+      cout << "Slicer: Min = " << _MinSlice << ", Max = " << _MaxSlice << std::endl;
+   }
+ 
+   void SetStatusMapper(vtkTextMapper* statusMapper) {
+      _StatusMapper = statusMapper;
+   }
+ 
+ 
+protected:
+   void MoveSliceForward() {
+      if(_Slice < _MaxSlice) {
+         _Slice += 1;
+         cout << "MoveSliceForward::Slice = " << _Slice << std::endl;
+         _ImageViewer->SetSlice(_Slice);
+         std::string msg = StatusMessage::Format(_Slice, _MaxSlice);
+         _StatusMapper->SetInput(msg.c_str());
+         _ImageViewer->Render();
+      }
+   }
+ 
+   void MoveSliceBackward() {
+      if(_Slice > _MinSlice) {
+         _Slice -= 1;
+         cout << "MoveSliceBackward::Slice = " << _Slice << std::endl;
+         _ImageViewer->SetSlice(_Slice);
+         std::string msg = StatusMessage::Format(_Slice, _MaxSlice);
+         _StatusMapper->SetInput(msg.c_str());
+         _ImageViewer->Render();
+      }
+   }
+ 
+ 
+   virtual void OnKeyDown() {
+      std::string key = this->GetInteractor()->GetKeySym();
+      if(key.compare("Up") == 0) {
+         //cout << "Up arrow key was pressed." << endl;
+         MoveSliceForward();
+      }
+      else if(key.compare("Down") == 0) {
+         //cout << "Down arrow key was pressed." << endl;
+         MoveSliceBackward();
+      }
+      // forward event
+      vtkInteractorStyleImage::OnKeyDown();
+   }
+ 
+ 
+   virtual void OnMouseWheelForward() {
+      //std::cout << "Scrolled mouse wheel forward." << std::endl;
+      MoveSliceForward();
+      // don't forward events, otherwise the image will be zoomed 
+      // in case another interactorstyle is used (e.g. trackballstyle, ...)
+      // vtkInteractorStyleImage::OnMouseWheelForward();
+   }
+ 
+ 
+   virtual void OnMouseWheelBackward() {
+      //std::cout << "Scrolled mouse wheel backward." << std::endl;
+      if(_Slice > _MinSlice) {
+         MoveSliceBackward();
+      }
+      // don't forward events, otherwise the image will be zoomed 
+      // in case another interactorstyle is used (e.g. trackballstyle, ...)
+      // vtkInteractorStyleImage::OnMouseWheelBackward();
+   }
+};
+ 
+vtkStandardNewMacro(myVtkInteractorStyleImage);
 
 int main(int argc, char *argv[]) {
     // --------------------
@@ -74,6 +180,55 @@ int main(int argc, char *argv[]) {
     vtkSmartPointer<vtkImageSlice> imageSlice = vtkSmartPointer<vtkImageSlice>::New();
     imageSlice->SetMapper(imageResliceMapper);
     imageSlice->GetProperty()->SetInterpolationTypeToNearest();
+
+    // --------------------
+    // VTK: (2D) Create a ImageViewer that uses input image.
+    // --------------------
+    vtkSmartPointer<vtkImageViewer2> imageViewer = vtkSmartPointer<vtkImageViewer2>::New();
+    imageViewer->SetInputData(conv->GetOutput()); 
+
+    // slice status message
+    vtkSmartPointer<vtkTextProperty> sliceTextProp = vtkSmartPointer<vtkTextProperty>::New();
+    sliceTextProp->SetFontFamilyToCourier();
+    sliceTextProp->SetFontSize(20);
+    sliceTextProp->SetVerticalJustificationToBottom();
+    sliceTextProp->SetJustificationToLeft();
+
+    vtkSmartPointer<vtkTextMapper> sliceTextMapper = vtkSmartPointer<vtkTextMapper>::New();
+    std::string msg = StatusMessage::Format(imageViewer->GetSliceMin(), imageViewer->GetSliceMax());
+    sliceTextMapper->SetInput(msg.c_str());
+    sliceTextMapper->SetTextProperty(sliceTextProp);
+
+    vtkSmartPointer<vtkActor2D> sliceTextActor = vtkSmartPointer<vtkActor2D>::New();
+    sliceTextActor->SetMapper(sliceTextMapper);
+    sliceTextActor->SetPosition(15, 10);    // create an interactor with our own style (inherit from vtkInteractorStyleImage)
+   // in order to catch mousewheel and key events
+   vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+      vtkSmartPointer<vtkRenderWindowInteractor>::New();
+ 
+   vtkSmartPointer<myVtkInteractorStyleImage> myInteractorStyle =
+      vtkSmartPointer<myVtkInteractorStyleImage>::New();
+ 
+   // make imageviewer2 and sliceTextMapper visible to our interactorstyle
+   // to enable slice status message updates when scrolling through the slices
+   myInteractorStyle->SetImageViewer(imageViewer);
+   myInteractorStyle->SetStatusMapper(sliceTextMapper);
+ 
+   imageViewer->SetupInteractor(renderWindowInteractor);
+   // make the interactor use our own interactorstyle
+   // cause SetupInteractor() is defining it's own default interatorstyle 
+   // this must be called after SetupInteractor()
+   renderWindowInteractor->SetInteractorStyle(myInteractorStyle);
+   // add slice status message and usage hint message to the renderer
+   imageViewer->GetRenderer()->AddActor2D(sliceTextActor);
+ 
+   // initialize rendering and interaction
+   //imageViewer->GetRenderWindow()->SetSize(400, 300);
+   //imageViewer->GetRenderer()->SetBackground(0.2, 0.3, 0.4);
+   imageViewer->Render();
+   imageViewer->GetRenderer()->ResetCamera();
+   imageViewer->Render();
+   renderWindowInteractor->Start();  
 
     // --------------------
     // VTK: Create RenderWindow and Renderer(s).
@@ -130,7 +285,7 @@ int main(int argc, char *argv[]) {
     // --------------------
     // VTK: Go!
     // --------------------
-    axialRenderer->AddViewProp(imageSlice);
+    //axialRenderer->AddViewProp(imageSlice);
     threeDRenderer->AddVolume(volume);
     sagittalRenderer->AddVolume(volume);
     coronalRenderer->AddVolume(volume);
