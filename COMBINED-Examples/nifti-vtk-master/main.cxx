e688a575d482b69b3c71e0bf3f8da1a304714b3c
Sam Esgate <sam.esgate11@imperial.ac.uk>
2015-01-08 19:01:56 +0000

    OK... didn't get the refactor done in time. Tomorrow's job now...

--- COMBINED-Examples/nifti-vtk-master/main.cxx
+++ COMBINED-Examples/nifti-vtk-master/main.cxx
@@ -22,7 +22,7 @@
 #include <vtkTextProperty.h>
 #include <vtkActor2D.h>
 
-#include <vtkObjectFactory.h>
+#include <myVtkInteractorStyleImage.h>
 
 const float BACKGROUND_R = 0.5f;
 const float BACKGROUND_G = 0.5f;
@@ -37,96 +37,6 @@ public:
       return tmp.str();
    }
 };
- 
- 
-// Define own interaction style
-class myVtkInteractorStyleImage : public vtkInteractorStyleImage
-{
-public:
-   static myVtkInteractorStyleImage* New();
-   vtkTypeMacro(myVtkInteractorStyleImage, vtkInteractorStyleImage);
- 
-protected:
-   vtkImageViewer2* _ImageViewer;
-   vtkTextMapper* _StatusMapper;
-   int _Slice;
-   int _MinSlice;
-   int _MaxSlice;
- 
-public:
-   void SetImageViewer(vtkImageViewer2* imageViewer) {
-      _ImageViewer = imageViewer;
-      _MinSlice = imageViewer->GetSliceMin();
-      _MaxSlice = imageViewer->GetSliceMax();
-      _Slice = _MinSlice;
-      cout << "Slicer: Min = " << _MinSlice << ", Max = " << _MaxSlice << std::endl;
-   }
- 
-   void SetStatusMapper(vtkTextMapper* statusMapper) {
-      _StatusMapper = statusMapper;
-   }
- 
- 
-protected:
-   void MoveSliceForward() {
-      if(_Slice < _MaxSlice) {
-         _Slice += 1;
-         cout << "MoveSliceForward::Slice = " << _Slice << std::endl;
-         _ImageViewer->SetSlice(_Slice);
-         std::string msg = StatusMessage::Format(_Slice, _MaxSlice);
-         _StatusMapper->SetInput(msg.c_str());
-         _ImageViewer->Render();
-      }
-   }
- 
-   void MoveSliceBackward() {
-      if(_Slice > _MinSlice) {
-         _Slice -= 1;
-         cout << "MoveSliceBackward::Slice = " << _Slice << std::endl;
-         _ImageViewer->SetSlice(_Slice);
-         std::string msg = StatusMessage::Format(_Slice, _MaxSlice);
-         _StatusMapper->SetInput(msg.c_str());
-         _ImageViewer->Render();
-      }
-   }
- 
- 
-   virtual void OnKeyDown() {
-      std::string key = this->GetInteractor()->GetKeySym();
-      if(key.compare("Up") == 0) {
-         //cout << "Up arrow key was pressed." << endl;
-         MoveSliceForward();
-      }
-      else if(key.compare("Down") == 0) {
-         //cout << "Down arrow key was pressed." << endl;
-         MoveSliceBackward();
-      }
-      // forward event
-      vtkInteractorStyleImage::OnKeyDown();
-   }
- 
- 
-   virtual void OnMouseWheelForward() {
-      //std::cout << "Scrolled mouse wheel forward." << std::endl;
-      MoveSliceForward();
-      // don't forward events, otherwise the image will be zoomed 
-      // in case another interactorstyle is used (e.g. trackballstyle, ...)
-      // vtkInteractorStyleImage::OnMouseWheelForward();
-   }
- 
- 
-   virtual void OnMouseWheelBackward() {
-      //std::cout << "Scrolled mouse wheel backward." << std::endl;
-      if(_Slice > _MinSlice) {
-         MoveSliceBackward();
-      }
-      // don't forward events, otherwise the image will be zoomed 
-      // in case another interactorstyle is used (e.g. trackballstyle, ...)
-      // vtkInteractorStyleImage::OnMouseWheelBackward();
-   }
-};
- 
-vtkStandardNewMacro(myVtkInteractorStyleImage);
 
 int main(int argc, char *argv[]) {
     // --------------------
