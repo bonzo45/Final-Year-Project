ae8c077c4a1021c530db34123cebd7ed68aee9ab
Sam Esgate <sam.esgate11@imperial.ac.uk>
2015-06-29 16:22:01 +0100

    Removed old examples/experimental code.

--- COMBINED-Examples/nifti-vtk-master/SliceInteractor/StatusMessage.cxx
+++
@@ -1,9 +0,0 @@
-#include <StatusMessage.h>
-#include <iostream>
-#include <sstream>
-
-std::string StatusMessage::Format(int slice, int maxSlice) {
-  std::stringstream tmp;
-  tmp << slice + 1 << "/" << maxSlice + 1;
-  return tmp.str();
-}
--- COMBINED-Examples/nifti-vtk-master/SliceInteractor/StatusMessage.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-
-// helper class to format slice status message
-class StatusMessage {
-public:
-   static std::string Format(int slice, int maxSlice);
-};
